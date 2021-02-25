c1b309320d07e5f8799d4caa394d3174e72ad626
w138789 <294496623@qq.com>
2021-02-25 10:23:22 +0800

    sss

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -20,7 +20,7 @@ class IndexController extends Controller
         return $this->response->success([
             'user' => $user,
             'method' => $method,
-            'message' => 'Hello syhahj Hyperf.',
+            'message' => 'Hello 没有的事 Hyperf.',
         ]);
     }
 }
