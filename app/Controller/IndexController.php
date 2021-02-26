4b8b3a3f18da593a886dac707ddb5053820d3f4d
w138789 <294496623@qq.com>
2021-02-26 13:44:05 +0800

    sss

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -17,6 +17,7 @@ class IndexController extends Controller
 {
     public function index()
     {
+        return 'sujia';
         $users = Db::table('goods')->get();//
         return $users;
     }
